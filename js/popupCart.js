603bc60a000a761e6baa13b80f2ee3ba75ea8db8
ShakirovaAliya <60111146+Shika8users.noreply.github.com>
2020-10-18 02:30:47 +0300

    исправления после консультации
    исправления после консультации

--- js/popupCart.js
+++ js/popupCart.js
@@ -1,12 +1,27 @@
-  const buy = document.querySelectorAll(".product__action--buy");
-  const mark = document.querySelectorAll(".product__action--add-to-bookmarks");
+  let productList = document.querySelector(".popular-products__list")
+  let buy = document.querySelectorAll(".product__action--buy");
+  let mark = document.querySelectorAll(".product__action--add-to-bookmarks");
   const сartPopup = document.querySelector(".modal-cart");
   const productNumber = 100;
   const buttonCloseCart = сartPopup.querySelector(".modal-close");
-  const cart = document.querySelector(".menu-item__link--cart");
-  const bookmark = document.querySelector(".menu-item__link--marks");
+  let cart = document.querySelector(".menu-item__link--cart");
+  let bookmark = document.querySelector(".menu-item__link--marks");
 
-  for (i=0; i <= productNumber; i++) {
+
+  productList.onclick = function(evt) {
+    let target = evt.target;
+    if(target.className != "product__action--buy") return;
+      сartPopup.classList.add("modal-show");
+      cart.style.backgroundColor = "#ee3643";
+  };
+
+  buttonCloseCart.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    сartPopup.classList.remove("modal-show");
+  });
+
+ /*
+  for (let i=0; i <= productNumber; i++) {
     buy[i].addEventListener("click", function(evt) {
       evt.preventDefault();
       сartPopup.classList.add("modal-show");
@@ -16,20 +31,20 @@
     buttonCloseCart.addEventListener("click", function(evt) {
       evt.preventDefault();
       сartPopup.classList.remove("modal-show");
-   });
+    });
 
     mark[i].addEventListener("click", function(evt) {
+     evt.preventDefault();
+     bookmark.style.backgroundColor = "#ee3643";
+});
+}
+*/
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (сartPopup.classList.contains("modal-show")) {
       evt.preventDefault();
-      bookmark.style.backgroundColor = "#ee3643";
-    });
-  }
-
-  window.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 27) {
-      if (сartPopup.classList.contains("modal-show")) {
-        evt.preventDefault();
-        сartPopup.classList.remove("modal-show");
-      }
+      сartPopup.classList.remove("modal-show");
     }
-  })
-
+  }
+})
