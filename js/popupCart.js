bb6786c437863bae211e40ea990170f16e35804f
ShakirovaAliya <60111146+Shika8users.noreply.github.com>
2020-10-19 01:09:45 +0300

    исправления по js
    исправления по js

--- js/popupCart.js
+++ js/popupCart.js
@@ -1,4 +1,5 @@
-  let productList = document.querySelector(".popular-products__list");
+  let productList = document.querySelector(".product-list");
+
   let catalogProductList = document.querySelector(".catalog-products__list");
   let buy = document.querySelectorAll(".product__action--buy");
   let mark = document.querySelectorAll(".product__action--add-to-bookmarks");
@@ -9,7 +10,7 @@
   let bookmark = document.querySelector(".menu-item__link--marks");
 
 
-  productList.onclick = function(evt) {
+  productList.addEventListener("click", function(evt) {
     let target = evt.target;
     if(target.classList.contains("product__action--buy")) {
       сartPopup.classList.add("modal-show");
@@ -17,32 +18,14 @@
     } else if(target.classList.contains("product__action--add-to-bookmarks")) {
       bookmark.style.backgroundColor = "#ee3643";
     };
-  };
+  });
+
 
   buttonCloseCart.addEventListener("click", function(evt) {
     evt.preventDefault();
     сartPopup.classList.remove("modal-show");
   });
 
- /*
-  for (let i=0; i <= productNumber; i++) {
-    buy[i].addEventListener("click", function(evt) {
-      evt.preventDefault();
-      сartPopup.classList.add("modal-show");
-      cart.style.backgroundColor = "#ee3643";
-    });
-
-    buttonCloseCart.addEventListener("click", function(evt) {
-      evt.preventDefault();
-      сartPopup.classList.remove("modal-show");
-    });
-
-    mark[i].addEventListener("click", function(evt) {
-     evt.preventDefault();
-     bookmark.style.backgroundColor = "#ee3643";
-});
-}
-*/
 
 window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
